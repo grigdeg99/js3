3512a81b9c2ee369463c7642bdc28302162c37b6
grigdeg99 <jun33@mail.ru>
2021-03-13 16:52:18 +0300

    33

--- js/script.js
+++ js/script.js
@@ -12,7 +12,17 @@ let appData = {
    savings: false
 };
 
-}
+for (let i = 0; i < 2; i++) {
+   let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+      b = prompt("Во сколько обойдется?", '');
+
+   if (typeof (a) === "string" && typeof (a) != null && typeof (b) != null &&
+      a != "" && b != "" && a.length < 50 && b.length < 50) {
+      console.log("done");
+      appData.expenses[a] + b;
+   } else {
+
+   }
 };
 // let i = 0;
 
